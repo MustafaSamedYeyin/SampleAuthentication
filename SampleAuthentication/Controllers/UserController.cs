868893eaf438b1493a04aa6515f2c3aee2a67091
Mustafa Samed Yeyin <mustafasamedyeyin@gmail.com>
2026-03-20 09:24:21 +0300

    Add project files.

---
+++ SampleAuthentication/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace SampleAuthentication.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetToken()
+        {
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("ThisIsASecretKeyForJwtTokenGenerationAndOnlyYouShouldKnotIt"));
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityTokenHandler().WriteToken(new JwtSecurityToken(
+                issuer: "localhost",
+                audience: "localhost",
+                claims: new[]
+                {
+                    new System.Security.Claims.Claim("userName", "Mustafa Samed")
+                },
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: signingCredentials));
+            return Ok(token);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetData() 
+        {
+            return Ok("This is proctected data.");
+        }
+    }
+}
---
+++ SampleAuthentication/Program.cs
@@ -0,0 +1,39 @@
+
+using Microsoft.IdentityModel.Tokens;
+
+namespace SampleAuthentication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+            builder.Services.AddAuthentication().AddJwtBearer(option =>
+            {
+                option.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "localhost",
+                    ValidAudience = "localhost",
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("ThisIsASecretKeyForJwtTokenGenerationAndOnlyYouShouldKnotIt")),
+                    ClockSkew = TimeSpan.FromSeconds(1)
+                };
+            });
+
+            var app = builder.Build();
+
+            app.MapOpenApi();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
